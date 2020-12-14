783511ad681dbd8aabe506caa5fc8f17d57e90fe
Nam SV <luotxem206@gmail.com>
2020-12-14 10:26:10 +0700

    test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\URL;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -23,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app['request']->server->set('HTTPS', true);
+        URL::forceScheme('https');
     }
 }
