c17f85de094cfafc24b6e45a1646cad6d8f92ccf
Thanh An <lvuthanhan1107@gmail.com>
2020-12-19 17:23:30 +0700

    an update

--- resources/views/calendar/layouts/app.blade.php
+++ resources/views/calendar/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="zxx">
+
 <head>
     <meta charset="UTF-8">
     <meta name="description" content="VKU meet for student's university">
@@ -8,14 +9,16 @@
     <title>@yield('title')</title>
     <link rel="icon" type="image/png" href="../image/LOGO.png" />
     <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Muli:300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:300,400,500,600,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Paytone+One&display=swap" rel="stylesheet">
+    <!-- Css Styles -->
+
+    <script src="{{ asset('JS/calendar.js')}}"></script>
     <link rel="stylesheet" type="text/css" href="{{asset('calendar/main.css')}}">
-  <link rel="stylesheet" type="text/css" href="{{asset('CSS/index.css')}}">
-  <script src="{{asset('calendar/main.js')}}"></script>
-
-  <link rel="stylesheet" type="text/css" href="calendar/main.css">
-  <link rel="stylesheet" type="text/css" href="CSS/index.css">
-  <script src="calendar/main.js"></script>
-
+    <link rel="stylesheet" type="text/css" href="{{asset('css/index.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/mymeeting.css')}}">
+    <script src="{{ asset('calendar/main.js')}}"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
