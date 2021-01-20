017e03c430f311e207ec969977fa40b40a2a3960
Thanh An <lvuthanhan1107@gmail.com>
2021-01-20 18:10:56 +0700

    update

--- resources/views/emails/Gmail.blade.php
+++ resources/views/emails/Gmail.blade.php
@@ -6,11 +6,11 @@
     <title>VKU Meet</title>
 </head>
 <body>
-    <h2>{{$details['title']}}</h1>
-    <h2>{{$details['body']}}</h2>
-    <h2>{{$details['title_calendar']}}</h2>
-    <h2>{{$details['time']}}</h2>
-    <h2>Xin Cảm Ơn!!!</h2>
-    <h2>Đây là Mail tự động, vui lòng không trả lời.</h2>
+    <h1>{{$details['title']}}</h1>
+    <h4>{{$details['body']}}</h4>
+    <h4>{{$details['title_calendar']}}</h4>
+    <h4>{{$details['time']}}</h4>
+    <h4>Xin Cảm Ơn!!!</h4>
+    <h4>Đây là Mail tự động, vui lòng không trả lời.</h4>
 </body>
 </html>
