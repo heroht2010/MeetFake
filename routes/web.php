fdd6a09c28bc084d156b586c2112bd43cdc75900
Thanh An <lvuthanhan1107@gmail.com>
2021-01-20 18:15:45 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -26,8 +26,8 @@ Route::get('/login/google', 'Auth\LoginController@redirectToProvider')->name('lo
 Route::get('/login/google/callback', 'Auth\LoginController@handleProviderCallback');
 
 //calendar
-Route::get('indexCalendar/{provider_id}','CalendarController@index');//->middleware('CheckLogin');
-Route::get('listEvent/{provider_id}','CalendarController@listEvent');//->middleware('CheckLogin');
+Route::get('indexCalendar/{provider_id}','CalendarController@index')->middleware('CheckLogin');
+Route::get('listEvent/{provider_id}','CalendarController@listEvent')->middleware('CheckLogin');
 Route::post('eventStore','CalendarController@store');
 Route::post('eventUpdate','CalendarController@update');
 Route::post('eventDelete','CalendarController@delete');
